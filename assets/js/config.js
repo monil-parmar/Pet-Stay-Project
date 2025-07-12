674c102c9a61e46ecf40922206508debd53a41f1
Monil Parmar <monilparmar27@gmail.com>
2025-07-11 22:00:00 -0400

    add details to config.js file and setup cognito

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.PETSTAY_CONFIG = {
   AWS_REGION: 'us-east-1',
   COGNITO_USER_POOL_ID: 'us-east-1_uTMQmx2SD',
   COGNITO_USER_POOL_CLIENT_ID: '5u02so0464u5ioglr5831g92k8',
-  COGNITO_DOMAIN: 'https://us-east-1utmqmx2sd.auth.us-east-1.amazoncognito.com',
+  COGNITO_DOMAIN: 'us-east-1utmqmx2sd.auth.us-east-1.amazoncognito.com',
   REDIRECT_SIGN_IN_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/admin-frontend/post-login.html',
   REDIRECT_SIGN_OUT_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/index.html',
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
