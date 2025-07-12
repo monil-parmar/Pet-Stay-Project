05818c2b5b766ff01d29e955beb2544837bc1f90
Monil Parmar <monilparmar27@gmail.com>
2025-07-12 01:54:15 -0400

    add details to config.js file and set up API Gateway

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,10 +5,10 @@ window.PETSTAY_CONFIG = {
   COGNITO_DOMAIN: 'us-east-1qixgkkfzg.auth.us-east-1.amazoncognito.com',
   REDIRECT_SIGN_IN_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/admin-frontend/post-login.html',
   REDIRECT_SIGN_OUT_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/index.html',
-  BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
-  ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
-  NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
-  CHECKIN_API_URL: "{{CHECKIN_API_URL}}"
+  BOOKINGS_API_URL: 'https://jv0ycwac6i.execute-api.us-east-1.amazonaws.com/bookings',
+  ROOMS_AVAILABILITY_API_URL: 'https://jv0ycwac6i.execute-api.us-east-1.amazonaws.com/rooms/availability',
+  NEW_BOOKING_API_URL: 'https://jv0ycwac6i.execute-api.us-east-1.amazonaws.com/newbooking',
+  CHECKIN_API_URL: 'https://jv0ycwac6i.execute-api.us-east-1.amazonaws.com/booking',
 };
 
 // Safety check: crash the page if placeholders were not replaced
