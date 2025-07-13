76a44340a1cf21f70b5836c67178410621a91f96
Monil Parmar <monilparmar27@gmail.com>
2025-07-13 01:18:20 -0400

    add details to config.js file and add SNS for lambda failure to admin

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,13 +5,16 @@ window.PETSTAY_CONFIG = {
   COGNITO_DOMAIN: 'us-east-1qixgkkfzg.auth.us-east-1.amazoncognito.com',
   REDIRECT_SIGN_IN_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/admin-frontend/post-login.html',
   REDIRECT_SIGN_OUT_URL: 'https://main.d3esln33qx1ws1.amplifyapp.com/index.html',
-  BOOKING_STATUS_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/bookingStatus',
+
+  BOOKINGS_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/bookings',          // All bookings
+  BOOKING_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/booking',            // Single booking by ID
+
   ROOMS_AVAILABILITY_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/rooms/availability',
   NEW_BOOKING_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/newbooking',
   CHECKIN_API_URL: 'https://24q261zi50.execute-api.us-east-1.amazonaws.com/booking',
 };
 
-// Safety check: crash the page if placeholders were not replaced
+// Safety check
 for (const key in window.PETSTAY_CONFIG) {
   if (window.PETSTAY_CONFIG[key].includes("{{") || window.PETSTAY_CONFIG[key].includes("}}")) {
     throw new Error(`Missing config value: ${key}. Did you forget to set environment variables?`);
